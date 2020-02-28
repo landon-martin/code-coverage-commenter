d61c48af74615e8ce86c5c9b558d64609f38bc37
Landon <landon.martin@bluescape.com>
2020-02-28 16:58:08 -0500

    Add linting and move files to src

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    test: true,
+    expect: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {
+  }
+}
