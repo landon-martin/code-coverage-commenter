ce76764894b56eeb3de44fc93bf5f0e9b7785747
landon-martin <55550837+landon-martin@users.noreply.github.com>
2020-03-03 09:35:25 -0500

    Switch to using @actions/exec (#5)
    * Switch to using @actions/exec
    
    * Check for GITHUB REF before trying to use it
    
    * Removing grabTableData export from index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { execSync } = require('child_process')
+const exec = require('@actions/exec')
 const { GitHub, context } = require('@actions/github')
 
 const grabTableData = require('./libs/grabTableData')
@@ -30,21 +30,38 @@ const main = async () => {
 
   const githubClient = new GitHub(gitHubToken)
 
-  const prNumber = getPrId(process.env.GITHUB_REF)
+  let fullReturn = ''
 
-  const fullReturn = execSync(covCommand).toString()
-  const codeCoverageTable = grabTableData(fullReturn)
+  const options = {}
+  options.listeners = {
+    stdout: (data) => {
+      fullReturn += data.toString()
+    },
+    stderr: (data) => {
+      fullReturn += data.toString()
+    }
+  }
+  await exec.exec(covCommand, options)
 
-  const commentBody = createComment(title, codeCoverageTable)
+  try {
+    if (process.env.GITHUB_REF) {
+      const prNumber = getPrId(process.env.GITHUB_REF)
+      const codeCoverageTable = grabTableData(fullReturn)
+      const commentBody = createComment(title, codeCoverageTable)
 
-  await githubClient.issues.createComment({
-    repo: repoName,
-    owner: repoOwner,
-    body: commentBody,
-    issue_number: prNumber
-  })
+      await githubClient.issues.createComment({
+        repo: repoName,
+        owner: repoOwner,
+        body: commentBody,
+        issue_number: prNumber
+      })
+    } else {
+      console.warn('GITHUB_REF not found, not commenting.')
+    }
+  } catch (e) {
+    console.error('Failed to create comment on PR')
+    console.error(e)
+  }
 }
 
 main().catch(err => core.setFailed(err.message))
-
-module.exports.grabTableData = grabTableData
